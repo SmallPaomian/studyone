7a0f32bd9b00ba2e77132840a79cc152ad7d954f
SmallPaomian <1319020857@qq.com>
2025-12-12 17:45:56 +0800

    three

--- two.py
+++ two.py
@@ -8,3 +8,14 @@ def max(a, b):
 a = 4
 b = 5
 print(max(a, b))
+
+def area(width, height):
+    return width * height
+ 
+def print_welcome(name):
+    print("Welcome", name)
+ 
+print_welcome("Runoob")
+w = 4
+h = 5
+print("width =", w, " height =", h, " area =", area(w, h))
